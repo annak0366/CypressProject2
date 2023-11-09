37e82e2d4dd4fedc1db06347cd5ad82b8965fde5
Annak0366 <annak0366@gmail.com>
2023-11-09 13:39:13 +0100

    third commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   projectId: "ojht16",
+  video: false,
+  reporterOptions: {
+
+    charts: true,
+
+    reportPageTitle: 'Cypress Inline Reporter',
+
+    embeddedScreenshots: true, 
+
+    inlineAssets: true, //Adds the asserts inline
+
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
